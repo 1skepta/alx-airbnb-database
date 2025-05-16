32c4c4bc30142b0a68b17f82fa862a2426d15f68
1skepta <prospaul999@gmail.com>
2025-05-16 12:23:08 +0000

    working

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -34,3 +34,18 @@ FROM
     users
 FULL OUTER JOIN 
     bookings ON users.id = bookings.user_id;
+
+
+-- 2. LEFT JOIN: Retrieve all properties and their reviews, including properties that have no reviews
+SELECT 
+    properties.id AS property_id,
+    properties.name AS property_name,
+    reviews.id AS review_id,
+    reviews.rating,
+    reviews.comment
+FROM 
+    properties
+LEFT JOIN 
+    reviews ON properties.id = reviews.property_id
+ORDER BY 
+    properties.id;
